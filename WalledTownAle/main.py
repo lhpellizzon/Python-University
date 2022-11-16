8752df0e775dc8fd754567ee6162644f7b973011
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-11-16 14:58:40 +0000

    Taking user personal details inputs

--- WalledTownAle/main.py
+++ WalledTownAle/main.py
@@ -1,1 +1,8 @@
-print("hello");
+print("Welcome to Walled Town Ale Order Platform\n".center(100))
+print("Please, complete the following fields:\n")
+
+# --- User Name and Address Inputs--- #
+companyName = input("Company Name:")
+street = input("Street Address:")
+town = input("Town/City:")
+county = input("County:")
