31638885f0d7761b9e06991657d5894e7b92f3f6
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-11-16 15:35:26 +0000

    Getting user inputs and creating invoice No. Striping all inputs needed to create Invoice e.g Name & Phone Number

--- WalledTownAle/main.py
+++ WalledTownAle/main.py
@@ -1,8 +1,15 @@
+import datetime
+
 print("Welcome to Walled Town Ale Order Platform\n".center(100))
 print("Please, complete the following fields:\n")
 
 # --- User Name and Address Inputs--- #
-companyName = input("Company Name:")
+companyName = input("Company Name:").strip()
+companyPhoneNumber = input("Phone Number:").strip()
 street = input("Street Address:")
 town = input("Town/City:")
 county = input("County:")
+actualDate = datetime.datetime.today().strftime('%d-%m-%Y')
+companyInvoiceNo = ("WTA" + companyPhoneNumber.lstrip("0")[0:2] + companyPhoneNumber[-2:] + companyName[-2]).upper()
+
+
